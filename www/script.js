4ab6255aa22d5bf8212a4f3cf2d028b1ea5128b1
tayyipozr <tayyipozr123@gmail.com>
2020-05-27 23:36:21 +0300

    quantity of items choosable and decresable

--- www/script.js
+++ www/script.js
@@ -3,11 +3,11 @@ var nnnn;
 var isEssential = false;
 
 class ShopItem {
-    constructor(id, name, quantity, isEssential) {
+    constructor(id, name, quantity = 1, isEssential) {
         this.id = id;
         this.name = name;
         this.isEssential = isEssential;
-        this.quantity = 0;
+        this.quantity = quantity;
     }
 }
 
@@ -35,7 +35,7 @@ class Storage {
         lists[nameOfList] = [];
         localStorage.setItem('lists', JSON.stringify(lists));
         console.log(lists);
-        document.querySelector('#need').innerHTML += `<ons-list-item modifier="material longdivider" id="tappableItem" tappable> <div class='left'>${nameOfList}</div> <div class='right'> <ons-row> <ons-col> <ons-button id='openPage2' width='20%' style='background-color: green; margin-right: 2px;'> Add </ons-button> </ons-col> <ons-col> <ons-button id='btnDelete' width='20%' style='background-color: red;'> Delete </ons-button> </ons-col> </ons-row> </div> </ons-list-item>`;
+        document.querySelector('#need').innerHTML += `<ons-list-item modifier="longdivider" id="tappableItem" tappable> <div class='left'>${nameOfList}</div> <div class='right'> <ons-row> <ons-col> <ons-button id='openPage2' width='20%' style='background-color: green; margin-right: 2px;'> Add </ons-button> </ons-col> <ons-col> <ons-button id='btnDelete' width='20%' style='background-color: red;'> Delete </ons-button> </ons-row> </ons-col> </div> </ons-list-item>`;
     }
 
     static displayLists() {
@@ -45,19 +45,20 @@ class Storage {
         // }
 
         Object.keys(lists).forEach((key) => {
-            document.querySelector('#need').innerHTML += `<ons-list-item modifier="material longdivider" id="tappableItem" tappable> <div class='left'>${key}</div> <div class='right'> <ons-row> <ons-col> <ons-button id='openPage2' width='20%' style='background-color: green; margin-right: 2px;'> Add </ons-button> </ons-col> <ons-col> <ons-button id='btnDelete' width='20%' style='background-color: red;'> Delete </ons-button> </ons-col> </ons-row> </div> </ons-list-item>`;
+            document.querySelector('#need').innerHTML += `<ons-list-item modifier="longdivider" id="tappableItem" tappable> <div class='left'>${key}</div> <div class='right'> <ons-row> <ons-col> <ons-button id='openPage2' width='20%' style='background-color: green; margin-right: 2px;'> Add </ons-button> </ons-col> <ons-col> <ons-button id='btnDelete' width='20%' style='background-color: red;'> Delete </ons-button> </ons-col> </ons-row> </div> </ons-list-item>`;
         });
 
     }
 
     static displayItems(listName) {
         let lists = Storage.getItems();
+        document.querySelector('#listName').innerHTML = listName;
         document.querySelector('#shopList').innerHTML = "";
         for (let [key, value] of Object.entries(lists)) {
             if (key == listName) {
                 value.forEach(item => {
-                    document.querySelector('#shopList').innerHTML == null ? console.log(null) : document.querySelector('#shopList').innerHTML += `<ons-list-item background-color: ${item.isEssential ? 'lightgreen' : 'lightblue'}; class='btn' id='item${i++}'> <div class='left' width='20%'> <ons-icon icon="fa-cart-arrow-down" class="list-item__icon"></ons-icon>
-                             </div>  <div class='center' width='60%'> - ${item.name} </div> <div class='right'> <ons-button data-id="${item.id}" id='btnDeleteItem' width='20%' style='background-color: red;'> Delete </ons-button> </div> </ons-list-item>`;
+                    document.querySelector('#shopList').innerHTML == null ? console.log(null) : document.querySelector('#shopList').innerHTML += `<ons-list-item modifier="longdivider" style="background-color: ${item.isEssential ? 'lightgreen' : 'lightblue'};" class='btn' id='item${i++}'> <div class='left' width='20%'> <ons-icon icon="fa-cart-arrow-down" class="list-item__icon"></ons-icon>
+                             </div>  <div class='center' width='60%'> - ${item.name} </div> <div class='right'> <ons-row style="margin-right: 10px;"> <div id="qua"> q : ${item.quantity}</div> </ons-row> <ons-row> <ons-button data-id="${item.id}" id='btnDeleteItem' width='20%' style='background-color: red;'> Delete </ons-button>  </ons-row> </div> </ons-list-item>`;
                 });
             }
         }
@@ -123,20 +124,24 @@ class Storage {
             let name;
             let id = target.getAttribute('data-id')
             items.forEach((item, index) => {
-                console.log(id);
-                console.log(item.id);
                 if (item.id == id) {
                     name = item.name;
-                    items.splice(index, 1);
-                    items[_key] = items;
-                    console.log(items[_key])
+                    if (item.quantity > 1) {
+                        items[_key] = new ShopItem(item.id, item.name, --item.quantity, item.isEssential);
+                        console.log(items[_key])
+                        localStorage.setItem('lists', JSON.stringify(lists));
+                        Storage.displayItems(listName);
+                    } else {
+                        items.splice(index, 1);
+                        items[_key] = items;
+                        console.log(items[_key])
+                        localStorage.setItem('lists', JSON.stringify(lists));
+                        // //writeFile(JSON.stringify(lists));
+                        Storage.displayItems(listName);
+                        ons.notification.toast(name + ' deleted from the list!', { timeout: 2000 });
+                    }
                 }
             });
-
-            localStorage.setItem('lists', JSON.stringify(lists));
-            Storage.displayItems(listName);
-            // //writeFile(JSON.stringify(lists));
-            ons.notification.toast(name + ' deleted from the list!', { timeout: 2000 });
         }
     }
 }
@@ -165,11 +170,16 @@ $(function() {
         const nameOfList = document.querySelector('#nameOfList');
 
         if (page.id == 'page2') {
-            isEssential = document.querySelector('.switch').addEventListener('change', essentialProduct);
+            isEssential = document.querySelector('ons-switch').addEventListener('change', essentialProduct);
+            let quantity = document.querySelector('#choose-sel');
+            let quantity_value;
+            quantity.addEventListener('change', function(e) {
+                quantity_value = e.target.value;
+            });
             saveButtonSecondPage.onclick = function() {
                 name = document.querySelector('#name').value;
                 let id = Date.now();
-                var item = new ShopItem(id, name, isEssential);
+                var item = new ShopItem(id, name, quantity_value, isEssential);
                 Storage.addItem(page.data.listName, item);
             }
         }
